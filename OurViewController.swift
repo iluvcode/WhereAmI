def8f01971c95006275b1f246ed1d54c27aef5f5
Myne <sandeeppalepu@gmail.com>
2015-11-27 01:13:35 -0500

    Initial Commit

---
+++ OurViewController.swift
@@ -0,0 +1,45 @@
+//
+//  OurViewController.swift
+//  WhereAmI
+//
+//  Created by Sandeep Palepu on 11/27/15.
+//  Copyright © 2015 Sandeep Palepu. All rights reserved.
+//
+
+import UIKit
+
+class OurViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        }
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WhereAmI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereAmI
+//
+//  Created by Sandeep Palepu on 11/27/15.
+//  Copyright © 2015 Sandeep Palepu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
